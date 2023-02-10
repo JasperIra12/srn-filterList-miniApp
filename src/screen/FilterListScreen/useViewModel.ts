e855c269a0773c913e95d128b32aea486f0c7292
Jasper Ira E Castillo <jasper.castillo@serino.com>
2023-02-10 15:53:37 +0800

    reset selected button

--- src/screen/FilterListScreen/useViewModel.ts
+++ src/screen/FilterListScreen/useViewModel.ts
@@ -11,7 +11,6 @@ export const useViewModel = ({ dataIn, dataLoad, dataOut }: Props) => {
     const [data, setData] = useState(dataLoad)
 
     const handlePress = (item: string) => {
-
         const updatedData = data.map((selected: any) => {
             if (item === 'All') {
                 const { isSelected } = selected
@@ -20,10 +19,9 @@ export const useViewModel = ({ dataIn, dataLoad, dataOut }: Props) => {
                 } else {
                     return { ...selected, isSelected: !isSelected };
                 }
-
             } else if (dataIn.multiSelect) {
                 if (selected.buttonName === item) {
-                    return { ...selected, isSelected: true };
+                    return { ...selected, isSelected: !selected.isSelected };
                 }
                 return selected;
             } else {
@@ -31,14 +29,12 @@ export const useViewModel = ({ dataIn, dataLoad, dataOut }: Props) => {
                     return { ...selected, isSelected: false };
                 }
                 if (selected.buttonName === item) {
-                    return { ...selected, isSelected: true };
+                    return { ...selected, isSelected: !selected.isSelected };
                 }
                 return selected;
             }
-
         });
         setData(updatedData);
-
         console.log(item)
     };
 
