f9fd8f9861b03d9b5021687b9e75aef39c8db2e6
Jasper Ira E Castillo <jasper.castillo@serino.com>
2023-02-13 09:33:43 +0800

    use lodash in useViewModel and add description on each function

--- README.md
+++ README.md
@@ -1,29 +1,32 @@
-# test-mini-app
+# srn-filter-list
 
 bootcamp
 
 ## Installation
 
 ```sh
-npm install test-mini-app
+npm install srn-filter-list
 ```
 
 ## Changelogs
 
 ```Changes per version
-v0.1.1 - add the dataIn, dataLoad, and dataOut props in readme
-v0.1.0 - miniapp first publish
+v0.1.5 - use lodash in useViewModel
+v0.1.4 - changes in readme
+v0.1.3 - changes in readme
+v0.1.2 - add the dataIn, dataLoad, and dataOut props in readme
+v0.1.1 - miniapp first publish
 ```
 
 ## FilterList Mini App DataIn Props
 
-| Prop                       | Required | Type         | Description                                  |
-| :------------------------- | :------- | :----------- | :------------------------------------------- |
-| **`showAllButton`**        | No       | `Boolean`    | if true, 'All' button will show              |
-| **`multiSelect`**          | No       | `Boolean`    | if true, you can select two or more buttons  |
-| **`sameWidth`**            | No       | `Boolean`    | if true, all button will have the same width |
-| **`activeButtonStyle`**    | No       | `View Style` | View style for all active buttons            |
-| **` inActiveButtonStyle`** | No       | `View Style` | View style for all inactive buttons          |
+| Prop                       | Required | Type         | Description                                            |
+| :------------------------- | :------- | :----------- | :----------------------------------------------------- |
+| **`showAllButton`**        | No       | `Boolean`    | if true, 'All' button will show                        |
+| **`multiSelect`**          | No       | `Boolean`    | if true, will allows you to select two or more buttons |
+| **`sameWidth`**            | No       | `Boolean`    | if true, all button will have the same width           |
+| **`activeButtonStyle`**    | No       | `View Style` | View style for all active buttons                      |
+| **` inActiveButtonStyle`** | No       | `View Style` | View style for all inactive buttons                    |
 
         |
 
@@ -35,7 +38,7 @@ v0.1.0 - miniapp first publish
 | **`buttonName`** | string  |
 | **`isSelected`** | boolean |
 
-## Horizontal and Vertical Data Out Props
+## FilterList Data Out Props
 
 | Prop                    | Required | Type         | Description          |
 | :---------------------- | :------- | :----------- | :------------------- |
--- src/screen/FilterListScreen/useViewModel.ts
+++ src/screen/FilterListScreen/useViewModel.ts
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import _ from 'lodash';
 import type { FilterListDataLoad, FilterListDataOut, FilterListDataIn } from 'src/types';
 
 type Props = {
@@ -13,56 +14,60 @@ export const useViewModel = ({ dataIn, dataLoad, dataOut }: Props) => {
 
     const handlePress = (item: any) => {
         let updatedData = [];
+        // If the pressed button is the 'All' button, update the selected status of all buttons
         if (item.buttonName === 'All') {
-            updatedData = data.map(selected => ({ ...selected, isSelected: !allIsSelected }));
+            updatedData = _.map(data, selected => ({ ...selected, isSelected: !allIsSelected }));
             allIsSelected ? dataOut([]) : dataOut(updatedData);
 
+            // If the 'All' button is not selected, set the data state to have all buttons (except 'All') as selected
             if (!allIsSelected) {
                 setData(
-                    updatedData.map(selected => ({
+                    _.map(updatedData, selected => ({
                         ...selected,
                         isSelected: selected.buttonName !== 'All'
                     }))
                 );
             }
         } else {
-            updatedData = data.map((selected: any) => {
+            // If the pressed button is not the 'All' button, update the selected status of the pressed button
+            updatedData = _.map(data, (selected: any) => {
                 if (selected.buttonName === item.buttonName) {
                     return { ...selected, isSelected: !selected.isSelected };
                 }
+                // If the 'All' button is selected, set its selected status to false
                 if (selected.buttonName === 'All') {
                     return { ...selected, isSelected: false };
                 }
                 return selected;
             });
 
-            // Check if multiselect is not enabled, then return the selected item
+            // If multiselect is not enabled, return the selected item
             if (!dataIn.multiSelect) {
-                const selectedItem = updatedData.find(selected => selected.isSelected === true);
+                const selectedItem = _.find(updatedData, selected => selected.isSelected === true);
                 dataOut(selectedItem || {});
                 if (!selectedItem) {
                     setData(updatedData);
                 } else {
                     setData(
-                        updatedData.map(selected => ({
+                        _.map(updatedData, selected => ({
                             ...selected,
                             isSelected: selected.buttonName === item.buttonName
                         }))
                     );
                 }
             } else {
-                dataOut(updatedData.filter(selected => selected.isSelected === true));
+                // If multiselect is enabled, return all selected items
+                dataOut(_.filter(updatedData, selected => selected.isSelected === true));
                 setData(updatedData);
             }
         }
-
-        const allButtonsSelected = updatedData.every(
+        // Check if all buttons (except 'All') are selected and update the 'All' button selected status accordingly
+        const allButtonsSelected = _.every(
+            updatedData,
             (selected: any) => selected.buttonName !== 'All' && selected.isSelected === true
         );
         setAllIsSelected(allButtonsSelected);
     };
-
-
 
     const buttons = dataIn.showAllButton
         ? [{ buttonName: 'All', isSelected: allIsSelected }, ...data]
